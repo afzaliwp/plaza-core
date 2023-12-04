0663b694184ba774f3fbe4d33837f30711c3dcd4
Mohammad Afzali <rahgozar1210@gmail.com>
2023-12-04 06:45:26 +0330

    PD-29 create the table of content and update gitignore

---
+++ assets/js/frontend/content-table.js
@@ -0,0 +1,43 @@
+const $ = jQuery;
+
+class ContentTable {
+
+	selectors = {
+		contentTableWrapper: '.content-table-wrapper',
+		content: '.single-post .contentstyle',
+		headings: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+	};
+
+	elements = {
+		content: document.querySelector(this.selectors.content),
+		contentTableWrapper: document.querySelector(this.selectors.contentTableWrapper)
+	};
+
+	constructor() {
+		if (!document.querySelector('main.single-post')) {
+			return;
+		}
+		if (!this.elements.contentTableWrapper) {
+			return;
+		}
+
+		this.handleTableContent();
+	}
+
+	handleTableContent() {
+		const headings = this.elements.content.querySelectorAll(this.selectors.headings.join(','));
+		let tocHTML = '<p>فهرست مطالب</p><ul>';
+
+		headings.forEach((heading, index) => {
+			const headingId = 'toc-' + (index + 1);
+			heading.setAttribute('id', headingId);
+
+			tocHTML += `<li><a href="#${ headingId }">${ heading.textContent }</a></li>`;
+		});
+		tocHTML += '</ul>';
+
+		this.elements.contentTableWrapper.innerHTML += tocHTML;
+	}
+}
+
+new ContentTable();
