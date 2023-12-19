13db25e0ab287f8d184ae90e2527cf004d393211
Mohammad Afzali <afzaliwp@gmail.com>
2023-12-19 07:41:52 +0330

    fix autoloader function

--- functions.php
+++ functions.php
@@ -4,7 +4,8 @@ function afzaliwp_plazadigital_autoload( $class_name ) {
 		return;
 	}
 
-	$file = str_replace( [ '_', 'AfzaliWP\PlazaDigital', '\\' ], [ '-', __DIR__, '/' ], $class_name ) . '.php';
+	$class_name = strtolower( $class_name );
+	$file = str_replace( [ '_', strtolower( 'AfzaliWP\PlazaDigital' ), '\\' ], [ '-', __DIR__, '/' ], $class_name ) . '.php';
 
-	require_once strtolower( $file );
+	require_once $file;
 }
