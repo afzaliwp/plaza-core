1cb66fa44484186ae714947510a33fa0e02706b4
Mohammad Afzali <afzaliwp@gmail.com>
2023-12-21 09:43:56 +0330

    PD-33 fix url not deleting the page part

--- includes/seo/canonical.php
+++ includes/seo/canonical.php
@@ -3,15 +3,17 @@
 namespace AfzaliWP\PlazaDigital\Includes\SEO;
 
 defined( 'ABSPATH' ) || die();
+
 class Canonical {
 	public function __construct() {
-		add_action('wp_head', [ 'set_canonical_url_paged_urls' ]);
+		add_action( 'wp_head', [ 'set_canonical_url_paged_urls' ] );
 	}
 
 	public function set_canonical_url_paged_urls() {
-		if (is_paged()) {
-			$url = get_pagenum_link(1);
-			echo '<link rel="canonical" href="' . esc_url($url) . '" />';
+		if ( is_paged() ) {
+			$url = get_pagenum_link( 1 );
+			preg_replace( '#/page/[0-9]+/?$#', '/', $url );
+			echo '<link rel="canonical" href="' . esc_url( $url ) . '" />';
 		}
 	}
 }
