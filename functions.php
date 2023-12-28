a4b92d07d3b931c0382a1f772d081c93aa446fc9
Mohammad Afzali <afzaliwp@gmail.com>
2023-12-28 10:59:20 +0330

    PD-30 fix dynamic shipping prices calculation

--- functions.php
+++ functions.php
@@ -6,5 +6,21 @@ function afzaliwp_plazadigital_autoload( $class_name ) {
 
 	$file = str_replace( [ '_', 'AfzaliWP\PlazaDigital', '\\' ], [ '-', __DIR__, '/' ], $class_name ) . '.php';
 
+<<<<<<< Updated upstream
 	require_once strtolower( $file );
+=======
+	require_once $file;
+}
+
+function mylog($data, $other_data = '') {
+	error_log( PHP_EOL . '-----------------------------------' );
+	error_log( '-------------$data: .'. $other_data .'---------------' );
+	if ( is_array($data) || is_object($data) ) {
+		error_log( print_r($data, true) );
+	} else {
+		error_log($data);
+	}
+	error_log( '-----------------------------------' );
+	error_log( '-----------------------------------'.PHP_EOL );
+>>>>>>> Stashed changes
 }
