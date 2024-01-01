6cc4056fbeacb5185ed4e07aaf666cd49544c452
Mohammad Afzali <afzaliwp@gmail.com>
2024-01-01 14:04:01 +0330

    PD-34 remove the test condition

--- includes/seo/schema.php
+++ includes/seo/schema.php
@@ -36,48 +36,18 @@ class Schema {
 				"priceValidUntil" => date( 'Y-m-d', strtotime( '+1 day' ) ),
 				"itemCondition" => "https://schema.org/NewCondition",
 				"availability" => $product->is_in_stock() ? "In Stock" : "Out of Stock",
+				"hasMerchantReturnPolicy" => true,
+				"returnPolicy" => [
+					"daysToReturn" => 15,
+					"returnInstructions" => "لطفاً با پشتیبانی تماس بگیرید تا روند بازگشت کالا را شروع کنید."
+				],
+				"shippingDetails" => [
+					"shippingMethod" => "پست سفارشی",
+					"deliveryTime" => "1-2 روز کاری",
+					"shippingLocations" => [ "تهران", "اصفهان", "شیراز" ]
+				]
 			],
 		];
-
-		if ( $product->get_id() === 94577 ) {
-			$schema = [
-				"@context" => "https://schema.org/",
-				"@type" => "Product",
-				"name" => $product->get_name(),
-				"image" => array_merge(
-					[ wp_get_attachment_url( $product->get_image_id() ) ],
-					array_map( function ( $image_id ) {
-						return wp_get_attachment_url( $image_id );
-					}, $product->get_gallery_image_ids() )
-				),
-				"description" => $product->get_description(),
-				"mpn" => $product->get_sku(),
-				"aggregateRating" => [
-					"@type" => "AggregateRating",
-					"ratingValue" => 0 == $product->get_average_rating() ? 5 : $product->get_average_rating(),
-					"reviewCount" => 0 == $product->get_review_count() ? 1 : $product->get_review_count(),
-				],
-				"offers" => [
-					"@type" => "Offer",
-					"url" => get_permalink(),
-					"priceCurrency" => 'IRR',
-					"price" => intval( $product->get_price() ) * 10,
-					"priceValidUntil" => date( 'Y-m-d', strtotime( '+1 day' ) ),
-					"itemCondition" => "https://schema.org/NewCondition",
-					"availability" => $product->is_in_stock() ? "In Stock" : "Out of Stock",
-					"hasMerchantReturnPolicy" => true,
-					"returnPolicy" => [
-						"daysToReturn" => 15,
-						"returnInstructions" => "لطفاً با پشتیبانی تماس بگیرید تا روند بازگشت کالا را شروع کنید."
-					],
-					"shippingDetails" => [
-						"shippingMethod" => "پست سفارشی",
-						"deliveryTime" => "1-2 روز کاری",
-						"shippingLocations" => [ "تهران", "اصفهان", "شیراز" ]
-					]
-				],
-			];
-		}
 
 		// Get the brand from product attribute
 		$brand = $product->get_attribute( 'brand' );
