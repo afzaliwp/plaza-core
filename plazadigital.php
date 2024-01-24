949de21d40fc1252ff08a2d2f288770a6a474d43
Mohammad Afzali <afzaliwp@gmail.com>
2024-01-24 14:15:30 +0330

    update version

--- plazadigital.php
+++ plazadigital.php
@@ -3,7 +3,7 @@
  * Plugin Name:       PlazaDigital Core
  * Plugin URI:        https://afzaliwp.com
  * Description:       Adds more functionality to the plaza digital website.
- * Version:           2.0.1
+ * Version:           2.0.2
  * Author:            Mohammad Afzali
  * Author URI:        https://afzaliwp.com
  */
@@ -176,7 +176,7 @@ final class PlazaDigital {
 			define( 'AFZALIWP_PD_ASSETS_VERSION', time() );
 		} else {
 			define( 'AFZALIWP_PD_IS_LOCAL', false );
-			define( 'AFZALIWP_PD_ASSETS_VERSION', '2.0.1' );
+			define( 'AFZALIWP_PD_ASSETS_VERSION', '2.0.2' );
 		}
 	}
 
