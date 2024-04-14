295e1bfc11a5a58646e7cfb9864255776a7d4dcc
Mohammad Afzali <afzaliwp@gmail.com>
2024-04-14 16:45:46 +0330

    add ask refund sms trigger to order preview

---
+++ assets/js/admin/order-preview-refund-info.js
@@ -0,0 +1,64 @@
+const $ = jQuery;
+
+class OrderPreviewRefundInfo {
+
+	selectors = {
+		wrapper: '.send-refund-info-order-preview',
+		sendButton: '.send-refund-info-button',
+		result: '.results',
+	}
+
+	elements = {
+		wrapper: '',
+		sendButton: '',
+		result: '',
+	}
+
+	constructor() {
+		this.handleSendSms();
+	}
+
+	handleSendSms() {
+		document.body.addEventListener('click', (event) => {
+			if (event.target.matches('.send-refund-info-button')) {
+				const button = event.target;
+				this.elements.wrapper = event.target.closest( this.selectors.wrapper );
+				this.elements.result = this.elements.wrapper.querySelector( this.selectors.result );
+				event.preventDefault();
+				button.disabled = true;
+				button.textContent = 'در حال ارسال';
+				this.elements.sendButton = button;
+
+				const orderId = button.dataset.orderId;
+				this.sendSms( orderId );
+			}
+		});
+	}
+
+	sendSms( orderId ) {
+		const self = this;
+		$.ajax( {
+			url: PlazaObj.ajaxUrl, // Replace with the actual URL of your AJAX handler
+			type: 'POST',
+			data: {
+				action: 'plaza_admin_send_refund_info_sms',
+				post_id: orderId,
+			},
+			success: function ( response ) {
+				self.elements.result.innerText = response.data;
+
+				self.elements.sendButton.disabled = false;
+				self.elements.sendButton.textContent = 'ارسال';
+			},
+			error: function () {
+				self.elements.result.innerText = 'خطا در ارسال';
+				self.elements.sendButton.disabled = false;
+				self.elements.sendButton.textContent = 'ارسال';
+			}
+		} );
+	}
+}
+
+window.addEventListener( 'load', () => {
+	new OrderPreviewRefundInfo();
+} );
--- includes/woocommerce/orders/order-preview.php
+++ includes/woocommerce/orders/order-preview.php
@@ -8,19 +8,39 @@ defined( 'ABSPATH' ) || die();
 
 class Order_Preview {
 
-	private $pattern = 'sendzpallink';
+	private $pattern = [
+		'zpal_link' => 'sendzpallink',
+		'refund_info' => 'askinforefund'
+	];
+
 	public function __construct() {
-		add_action( 'wp_ajax_plaza_admin_send_zpal_link_sms', [ $this, 'handle_sms_delivery' ] );
+		add_action( 'wp_ajax_plaza_admin_send_zpal_link_sms', [ $this, 'handle_zpal_link_sms_delivery' ] );
+		add_action( 'wp_ajax_plaza_admin_send_refund_info_sms', [ $this, 'handle_refund_info_sms_delivery' ] );
 		add_action( 'woocommerce_admin_order_preview_start', [ $this, 'order_preview_data' ] );
 	}
 
-	public function handle_sms_delivery() {
+	public function handle_zpal_link_sms_delivery() {
 		$order = wc_get_order( $_POST[ 'post_id' ] );
 		$phone_number = $order->get_billing_phone();
 
 		// Send SMS
 		$sms = new KaveNegar();
-		$response = $sms->lookup( $phone_number, $this->pattern, $_POST['post_id'] );
+		$response = $sms->lookup( $phone_number, $this->pattern['zpal_link'], $_POST[ 'post_id' ] );
+		$sms_status = $response[ 'status' ] ? 'SMS sent successfully: ' . $response[ 'message' ] : 'Failed to send SMS: ' . $response[ 'message' ];
+
+		$sms_status .= ' - ' . date( 'Y-m-d H:i:s' ); // Append the current date and time
+
+		wp_send_json_success( $sms_status );
+	}
+
+
+	public function handle_refund_info_sms_delivery() {
+		$order = wc_get_order( $_POST[ 'post_id' ] );
+		$phone_number = $order->get_billing_phone();
+
+		// Send SMS
+		$sms = new KaveNegar();
+		$response = $sms->lookup( $phone_number, $this->pattern['refund_info'], $_POST[ 'post_id' ] );
 		$sms_status = $response[ 'status' ] ? 'SMS sent successfully: ' . $response[ 'message' ] : 'Failed to send SMS: ' . $response[ 'message' ];
 
 		$sms_status .= ' - ' . date( 'Y-m-d H:i:s' ); // Append the current date and time
@@ -32,16 +52,26 @@ class Order_Preview {
 		global $post;
 
 		?>
-		<div class="send-zpal-link-order-preview">
+        <div class="send-zpal-link-order-preview">
             <h4>جهت ارسال پیامک لینک زرین پال برای پرداخت بیعانه، روی دکمه زیر کلیک کنید.</h4>
-			<button
+            <button
                     data-order-id="<?php echo $post->ID; ?>"
                     class="button button-primary send-zpal-link-button">
                 ارسال پیامک لینک زرین پال
             </button>
 
             <div class="results"></div>
-		</div>
+        </div>
+        <div class="send-refund-info-order-preview">
+            <h4>جهت ارسال پیامک درخواست تکمیل فرم عودت وجه، روی دکمه زیر کلیک کنید.</h4>
+            <button
+                    data-order-id="<?php echo $post->ID; ?>"
+                    class="button button-primary send-refund-info-button">
+                ارسال پیامک تکمیل فرم عودت وجه
+            </button>
+
+            <div class="results"></div>
+        </div>
 		<?php
 	}
 }
